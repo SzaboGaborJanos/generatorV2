8fb08b77baf57a0c9ab67f967869ba59a7cfa28a
szabo <szabogabor@weboldalnet.hu>
2024-09-11 17:57:30 +0200

    v2

--- main.c
+++ main.c
@@ -1,99 +1,96 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h> // system("cls") miatt
 
+void createMain(int num) {
+    int i;
+    FILE *file;
+    file = fopen("feladatok.txt", "w");
+    fprintf(file, "#include <stdio.h>\n");
+    fprintf(file, "#include <stdlib.h> // system(\"cls\") miatt\n\n");
 
-void feladat1();
-void feladat2();
-void feladat3();
-void feladat4();
-void feladat5();
+    // Függvények deklarálása
+    for (i = 1; i <= num; i++) {
+        fprintf(file, "\nvoid feladat%d();", i);
+    }
+    fprintf(file, "\n\n");
+
+    // main függvény generálása
+    fprintf(file, "int main() {\n");
+    fprintf(file, "    int feladatszam;\n");
+    fprintf(file, "    char valasz;\n");
+    fprintf(file, "    int ujra;\n");
+    fprintf(file, "    do {\n");
+    fprintf(file, "        system(\"cls\"); // Konzol törlése\n");
+    fprintf(file, "        printf(\"Feladatok:\\n\");\n\n");
+
+    for (i = 1; i <= num; i++) {
+        fprintf(file, "        printf(\"\\t%d.feladat: - \\n\");\n", i);
+    }
+
+    fprintf(file, "\n        printf(\"Melyik feladatot szeretne megnezni?: \");\n");
+    fprintf(file, "        scanf(\"%%d\", &feladatszam);\n\n");
+
+    fprintf(file, "        if (feladatszam >= 1 && feladatszam <= %d) {\n", num);
+    fprintf(file, "            switch (feladatszam) {\n");
+    for (i = 1; i <= num; i++) {
+        fprintf(file, "                case %d:\n", i);
+        fprintf(file, "                    feladat%d();\n", i);
+        fprintf(file, "                    break;\n");
+    }
+    fprintf(file, "                default:\n");
+    fprintf(file, "                    printf(\"Nem letezik ilyen szamu feladat!\\n\");\n");
+    fprintf(file, "                    break;\n");
+    fprintf(file, "            }\n");
+    fprintf(file, "        } else {\n");
+    fprintf(file, "            printf(\"Nem letezik ilyen szamu feladat!\\n\");\n");
+    fprintf(file, "        }\n\n");
+
+    fprintf(file, "        while (1) {\n");
+    fprintf(file, "            printf(\"Szeretne egy masik feladatot megnezni? (y/n): \");\n");
+    fprintf(file, "            scanf(\" %%c\", &valasz);\n");
+    fprintf(file, "            if (valasz == 'y' || valasz == 'Y') {\n");
+    fprintf(file, "                ujra = 1;\n");
+    fprintf(file, "                break;\n");
+    fprintf(file, "            } else if (valasz == 'n' || valasz == 'N') {\n");
+    fprintf(file, "                ujra = 0;\n");
+    fprintf(file, "                break;\n");
+    fprintf(file, "            } else {\n");
+    fprintf(file, "                printf(\"Ervenytelen valasz! Kerem adjon meg 'y' vagy 'n'.\\n\");\n");
+    fprintf(file, "            }\n");
+    fprintf(file, "        }\n");
+    fprintf(file, "    } while (ujra);\n\n");
+
+    fprintf(file, "    return 0;\n");
+    fprintf(file, "}\n");
+
+    // Függvények definíciói
+    for (i = 1; i <= num; i++) {
+        fprintf(file, "\nvoid feladat%d() {\n", i);
+        fprintf(file, "        system(\"cls\"); // Konzol törlése\n");
+        fprintf(file, "    printf(\"%d.feladat\\n\");\n", i);
+        fprintf(file, "    // Feladat %d megoldasa\n", i);
+        fprintf(file, "}\n");
+    }
+
+    fclose(file);
+}
 
 int main() {
-    int feladatszam;
-    char valasz;
-    int ujra;
-    do {
-        system("cls"); // Konzol törlése
-        printf("Feladatok:\n");
+    int num;
+    printf("Hany feladatot szeretne generalni? : ");
+    scanf("%d", &num);
 
-        printf("\t1.feladat: - \n");
-        printf("\t2.feladat: - \n");
-        printf("\t3.feladat: - \n");
-        printf("\t4.feladat: - \n");
-        printf("\t5.feladat: - \n");
-
-        printf("Melyik feladatot szeretne megnezni?: ");
-        scanf("%d", &feladatszam);
-
-        if (feladatszam >= 1 && feladatszam <= 5) {
-            switch (feladatszam) {
-                case 1:
-                    feladat1();
-                    break;
-                case 2:
-                    feladat2();
-                    break;
-                case 3:
-                    feladat3();
-                    break;
-                case 4:
-                    feladat4();
-                    break;
-                case 5:
-                    feladat5();
-                    break;
-                default:
-                    printf("Nem letezik ilyen szamu feladat!\n");
-                    break;
-            }
-        } else {
-            printf("Nem letezik ilyen szamu feladat!\n");
-        }
-
-        while (1) {
-            printf("Szeretne egy masik feladatot megnezni? (y/n): ");
-            scanf(" %c", &valasz);
-            if (valasz == 'y' || valasz == 'Y') {
-                ujra = 1;
-                break;
-            } else if (valasz == 'n' || valasz == 'N') {
-                ujra = 0;
-                break;
-            } else {
-                printf("Ervenytelen valasz! Kerem adjon meg 'y' vagy 'n'.\n");
-            }
-        }
-    } while (ujra);
+    if (num >= 1) {
+        createMain(num);
+        printf("A fajl letrehozva: feladatok.txt\n");
+        printf("Bezrahatja a programot (Nyomjon ENTER-t)...\n");
+        _getch();
+    } else {
+        printf("Ervenytelen bemenet! Adjon meg egy szamot ami 1-nel nagyobb.\n");
+        printf("Bezrahatja a programot (Nyomjon ENTER-t)...\n");
+        _getch();
+    }
 
     return 0;
-}
-
-void feladat1() {
-        system("cls"); // Konzol törlése
-    printf("1.feladat\n");
-    // Feladat 1 megoldasa
-}
-
-void feladat2() {
-        system("cls"); // Konzol törlése
-    printf("2.feladat\n");
-    // Feladat 2 megoldasa
-}
-
-void feladat3() {
-        system("cls"); // Konzol törlése
-    printf("3.feladat\n");
-    // Feladat 3 megoldasa
-}
-
-void feladat4() {
-        system("cls"); // Konzol törlése
-    printf("4.feladat\n");
-    // Feladat 4 megoldasa
-}
-
-void feladat5() {
-        system("cls"); // Konzol törlése
-    printf("5.feladat\n");
-    // Feladat 5 megoldasa
 }
